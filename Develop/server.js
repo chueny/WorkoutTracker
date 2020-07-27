b84251515045d572ac1b0cd5be457a9238111655
Chue Yang <chueyangib@gmail.com>
2020-07-26 22:55:20 -0500

    nothing

--- Develop/server.js
+++ Develop/server.js
@@ -20,14 +20,6 @@ app.use(cors());
 
 app.use(express.static("public"));
 
-// API.JS ROUTES GO HERE ===========
-//do I need this?
-app.get("/", (req, res)=>{
-    //think i have to send a file to this site.
-    //send a path or a html file...?
-    //Object Document Mapper = MangoDo
-});
-
 //get last workout
 app.get("/api/workouts", (req, res) => {
     Workout.find({}).then(data =>{
@@ -40,46 +32,50 @@ app.get("/api/workouts", (req, res) => {
     //res.json({data: "Hello"});
 });
 
-//looks like there is a app.put for the same /api/workouts
-//put - update
-//post - add it
-app.post("/api/workouts/:id", (req, res)=>{
 
-    console.log(req.body);
-    res.send(res.body);
-    const { day, exercise, type, name, weight, duration } = req.body;
+app.put("/api/workouts/:id", (req, res)=>{
+
+    // console.log(req.body);
+    // res.send(res.body);
+    const { day, exercises, type, name, weight, reps, sets, duration, distance } = req.body;
     
-    // Workout.update(
-    // {
-    //     _id: mongojs.ObjectId(req.params.id)
-    // },
-    // {
-    //  $set: 
-    //     { 
-    //     type: req.body.type, 
-    //     name: req.body.name,
-    //     weight: req.body.weight
-    //     }
-        
-    //          body??? ///WHAT?// IDENTIFY WHERE  ADD EXERCISE, iD what code is triggered. 
-    //     model definte document in MOngo. 
-    //     still need to wrap data in model. 
-    //     need to create model object and then save to databse
-    // }
-    // ), (err, data) => {
-    // if (err){
-    //     console.log(err);
-    // }else{
-    //     res.send(data);
-    // }};
-
+    Workout.update(
+    {
+        _id: mongojs.ObjectId(req.params.id)
+    },
+    {
+     $set: 
+        { day,
+        exercises:[
+        type,
+        name,
+        weight, 
+        reps,
+        sets,
+        duration,
+        distance 
+        ]
+        // type: req.body.type, 
+        // name: req.body.name,
+        // weight: req.body.weight,
+        // reps: req.body.weight,
+        // sets: req.body.sets,
+        // duration: req.body.duration,
+        // distance: req.body.distance
+        }
+    }), (err, data) => {
+    if (err){
+        console.log(err);
+    }else{
+        res.send(data);
+    }};
 });
 
 //create a workout
 app.post("/api/workouts", (req, res)=>{
     Workout.insert({body}, (err, data) =>{
         if (err){
-
+            console.log(err);
         }else {
             res.json(data);
         }
@@ -89,47 +85,14 @@ app.post("/api/workouts", (req, res)=>{
 
 //getWorkoutsinRange
 app.get("/api/workouts/range", (req, res) => {
-    Workout.find({}, (err, data)=>{
+    
+    Workout.find({}).then(data =>{
         res.json(data);
-
+    })
+    .catch(err => {
+        res.json(err);
     });
 });
-
-//exercise.js ROUTES GO HERE =======
-//is this a app.post for initExercise
-app.post("/:id", (req, res)=>{
-    Workout.update()
-
-});
-
-
-//index.js ROUTES GO HERE ===========
-app.get("/:id", (req,res)=>{
-    Workout.find(
-        {
-            _id: mongojs.ObjectId(req.params.id)
-        },(err, data) =>{
-            if (err){
-                console.log(err);
-            } else{
-                res.send(data);
-            }
-        });
-});
-
-//stats.js ROUTES GO HERE ========
-//getlastwOrkout
-
-app.get("/exercise/:id", (req, res)=>{
-
-    Workout.find({_id: mongojs.ObjectId(req.param.id)}, (err, data) => {
-
-    });
-
-});
-
-
-
 
 db.once("open", function (){
     app.listen(PORT, () => {
